9fcbece6db9d1c73567cf94c7b7567523a02455e
MehakK1311 <mehakhanna1311@gmail.com>
2025-01-09 00:04:17 +0530

    user auth

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import authRouter from "./routes/auth.route.js"
+
+
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to mongoDB")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({ origin: [process.env.CLIENT_URL], credentials: true }))
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000")
+})
+
+
+
+app.use("/api/auth", authRouter)
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500
+  const message = err.message || "Internal Serer Error"
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  })
+})
