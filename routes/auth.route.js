fca126edf65b796b7cc7e1978b36ea496302594a
MehakK1311 <mehakhanna1311@gmail.com>
2025-01-09 15:21:42 +0530

    fix

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { signin, signout, signup } from "../controller/auth.controller.js"
-import { verifyToken } from "../utils/VErifyUser.js"
+import { verifyToken } from "../utils/verifyUser.js"
 
 
 const router = express.Router()
