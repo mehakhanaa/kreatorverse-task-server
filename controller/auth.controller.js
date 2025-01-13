f5a436a3c02d75769233bed97ff181b94d1a771a
Mehak Khanna <khannamehak63425@gmail.com>
2025-01-13 17:54:40 +0000

    fix: fix

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -52,11 +52,7 @@ export const signin = async (req, res, next) => {
 
     const { password: pass, ...rest } = validUser._doc
 
-    res.cookie("access_token", token, { 
-      httpOnly: true,
-      secure: true,
-      sameSite: "None"
-     }).status(200).json({
+    res.cookie("access_token", token).status(200).json({
       success: true,
       message: "Login Successful!",
       data: rest,
--- index.js
+++ index.js
@@ -22,10 +22,11 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: "*",
+  origin: "https://kreatorverse-task-server-production.up.railway.app",
   credentials: true,
-  optionsSuccessStatus: 200,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+
 }))
 app.listen(3000, () => {
   console.log("Server is running on port 3000")
