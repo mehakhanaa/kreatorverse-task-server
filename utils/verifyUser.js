07749707c9f79ae693fabb3a669527d831303dfb
Mehak Khanna <khannamehak63425@gmail.com>
2025-01-13 18:16:17 +0000

    fix: fix

--- utils/verifyUser.js
+++ utils/verifyUser.js
@@ -3,7 +3,8 @@ import jwt from "jsonwebtoken"
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token
-
+  const stoken = req.signedCookies.access_token
+  console.log("THIS IS ", token, ":::", stoken)
   if (!token) {
     return next(errorHandler(401, "Unauthorized"))
   }
