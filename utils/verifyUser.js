46b0cfa5ce63c685dc6c2865ae425afd21d8b0b3
MehakK1311 <mehakhanna1311@gmail.com>
2025-01-13 20:40:56 +0530

    test error

--- utils/verifyUser.js
+++ utils/verifyUser.js
@@ -4,6 +4,8 @@ import jwt from "jsonwebtoken"
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token
 
+  console.log(token)
+
   if (!token) {
     return next(errorHandler(401, "Unauthorized"))
   }
