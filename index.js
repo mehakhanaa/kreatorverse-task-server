977590d27b07496f45c5a2424d18300886917c9b
MehakK1311 <mehakhanna1311@gmail.com>
2025-01-13 22:25:47 +0530

    cors error

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: "*",
   credentials: true,
   optionsSuccessStatus: 200,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
